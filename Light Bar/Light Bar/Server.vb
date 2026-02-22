e9d10b96765d71a6c081c8f7f00114cd63f09752
kaanavean <denizkaandivrikli@gmail.com>
2026-02-22 02:06:05 +0100

    Updated Server.vb

--- Light Bar/Light Bar/Server.vb
+++ Light Bar/Light Bar/Server.vb
@@ -9,7 +9,6 @@ Public Class MenuServer
     <DllImport("user32.dll")> Private Shared Function GetForegroundWindow() As IntPtr : End Function
     <DllImport("user32.dll")> Private Shared Function GetWindowThreadProcessId(hWnd As IntPtr, ByRef lpdwProcessId As Integer) As Integer : End Function
 
-    ' Events für die Desktop.exe UI
     Public Event RequestMenuUpdate(items As List(Of MenuItemData))
     Public Event RequestDefaultMenu()
 
@@ -18,24 +17,19 @@ Public Class MenuServer
     Private _myPID As Integer = Process.GetCurrentProcess().Id
 
     Public Sub Start()
-        ' Thread für den Empfang von Menü-Strukturen
         Dim t As New Thread(AddressOf ListenForApps)
         t.IsBackground = True
         t.Start()
     End Sub
 
-    ' Muss vom Timer der Desktop.exe (z.B. alle 200ms) aufgerufen werden
     Public Sub CheckFocus()
         Dim hwnd As IntPtr = GetForegroundWindow()
         Dim procID As Integer
         GetWindowThreadProcessId(hwnd, procID)
 
-        ' 1. Spezialfall: Wenn wir selbst den Fokus haben, 
-        ' prüfen wir NUR, ob die letzte App noch lebt.
         If procID = _myPID Then
             If _lastProcessID <> -1 Then
                 If Not IsProcessRunning(_lastProcessID) Then
-                    ' Die App wurde geschlossen, während wir das Menü offen hatten!
                     _appMenus.Remove(_lastProcessID)
                     _lastProcessID = -1
                     RaiseEvent RequestDefaultMenu()
@@ -44,7 +38,6 @@ Public Class MenuServer
             Exit Sub
         End If
 
-        ' 2. Normaler Fokus-Check für andere Fenster
         If procID <> _lastProcessID Then
             _lastProcessID = procID
             If _appMenus.ContainsKey(procID) Then
@@ -52,17 +45,15 @@ Public Class MenuServer
                     RaiseEvent RequestMenuUpdate(_appMenus(procID))
                 Else
                     _appMenus.Remove(procID)
-                    _lastProcessID = -1 ' Reset, da Prozess tot
+                    _lastProcessID = -1
                     RaiseEvent RequestDefaultMenu()
                 End If
             Else
-                ' Fokus liegt auf einem Fenster ohne dynamisches Menü (z.B. Browser)
                 RaiseEvent RequestDefaultMenu()
             End If
         End If
     End Sub
 
-    ' In Server.vb der DLL innerhalb von ListenForApps
     Private Sub ListenForApps()
         While True
             Try
@@ -71,12 +62,8 @@ Public Class MenuServer
                     Using reader As New StreamReader(server)
                         Dim json = reader.ReadToEnd()
                         Dim package = JsonSerializer.Deserialize(Of MenuPackage)(json)
-
                         _appMenus(package.ProcessID) = package.MenuItems
-
-                        ' NEU: Den Prozess aktiv überwachen
                         WatchProcess(package.ProcessID)
-
                         If package.ProcessID = _lastProcessID Then
                             RaiseEvent RequestMenuUpdate(package.MenuItems)
                         End If
@@ -100,14 +87,12 @@ Public Class MenuServer
         Catch : End Try
     End Sub
 
-    ' In Server.vb der DLL
     Public Sub SendClickToActiveApp(commandID As String)
         If _lastProcessID = -1 Then Exit Sub
 
         Dim targetPID = _lastProcessID
         Task.Run(Sub()
                      Try
-                         ' Timeout auf 100ms setzen, damit die Leiste nicht einfriert
                          Using client As New NamedPipeClientStream(".", "MacOSClickPipe_" & targetPID, PipeDirection.Out)
                              client.Connect(100)
                              Using writer As New StreamWriter(client)
@@ -116,8 +101,6 @@ Public Class MenuServer
                              End Using
                          End Using
                      Catch ex As Exception
-                         ' Wenn die App nicht antwortet (weil sie schließt), 
-                         ' triggern wir sofort das Standard-Menü
                          _appMenus.Remove(targetPID)
                          RaiseEvent RequestDefaultMenu()
                      End Try
